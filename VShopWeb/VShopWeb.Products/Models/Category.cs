fcf409e343d6beb79241358bef7bf87b3b60374f
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-05 15:22:01 -0300

    entities created

---
+++ VShopWeb/VShopWeb.Products/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace VShopWeb.Products.Models;
+
+public class Category
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public ICollection<Product>? Products { get; set; }
+}
