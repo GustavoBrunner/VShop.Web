ecfe6d5e7ac004bff5100ca694ba2d9d11758c46
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-05 19:23:03 -0300

    unit of work pattern added

---
+++ VShopWeb/VShopWeb.Products/Repository/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using VShopWeb.Products.Infra;
+using VShopWeb.Products.Repository.Contracts;
+
+namespace VShopWeb.Products.Repository;
+
+public class UnitOfWork : IUnityOfWork
+{
+    private readonly ApiDbContext apiDbContext;
+    private bool _disposed = false;
+    public UnitOfWork(ICategoryRepository categoryRepository, ApiDbContext apiDbContext)
+    {
+        CategoryRepository = categoryRepository;
+        this.apiDbContext = apiDbContext;
+    }
+
+    public IProductRepository ProductRepository { get; private set ; }
+    public ICategoryRepository CategoryRepository { get; private set; }
+
+    public async Task<bool> Commit()
+    {
+        return await apiDbContext.SaveChangesAsync() > 0;
+    }
+    protected virtual void Dispose(bool disposing) 
+    {
+        if(_disposed) return;
+        if (disposing)
+        {
+            apiDbContext.Dispose();
+            _disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
