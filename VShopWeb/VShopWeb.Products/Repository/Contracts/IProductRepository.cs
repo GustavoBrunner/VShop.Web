3ea59ebfd2f14891b954714cbcbd53c1ec17c5b9
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-05 18:10:43 -0300

    product repository implementation

--- VShopWeb/VShopWeb.Products/Repository/Contracts/IProductRepository.cs
+++ VShopWeb/VShopWeb.Products/Repository/Contracts/IProductRepository.cs
@@ -7,8 +7,8 @@ public interface IProductRepository
     Task<Product> GetById(string id);
     Task<IEnumerable<Product>> GetAll();
     Task<IEnumerable<Product>> GetAllWithCategory();
-    Task<Product> Create(Product product);
-    Task<Product> Update(Product product);
-    Task<Product> Delete(Product product);
+    Task Create(Product product);
+    Task Update(Product product);
+    Task Delete(Product product);
 
 }
--- VShopWeb/VShopWeb.Products/Repository/ProductRepository.cs
+++ VShopWeb/VShopWeb.Products/Repository/ProductRepository.cs
@@ -1,5 +1,53 @@
-﻿namespace VShopWeb.Products.Repository;
+﻿using Microsoft.EntityFrameworkCore;
+using VShopWeb.Products.Infra;
+using VShopWeb.Products.Models;
+using VShopWeb.Products.Repository.Contracts;
 
-public class ProductRepository
+namespace VShopWeb.Products.Repository;
+
+public class ProductRepository : IProductRepository
 {
+    private readonly ApiDbContext _context;
+
+    public ProductRepository(ApiDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Create(Product product)
+    {
+        await _context.Products.AddAsync(product);
+    }
+
+    public async Task Delete(Product product)
+    {
+        var entity = await GetById(product.Id);
+        _context.Entry(entity).State = EntityState.Deleted;
+    }
+
+
+    public async Task Update(Product product)
+    {
+        var entity = await GetById(product.Id);
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+    public async Task<Product> GetById(string id)
+    {
+        return await _context.Products
+            .FirstOrDefaultAsync(p => p.Id == id);    
+    }
+
+    public async Task<IEnumerable<Product>> GetAll()
+    {
+        return await _context.Products
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetAllWithCategory()
+    {
+        return await _context.Products
+            .Include(p => p.Category)
+            .ToListAsync();
+    }
+
 }
