6e24e75c5235c519371567ab18ca27d17a6253b6
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-05 19:55:34 -0300

    product service interface added

---
+++ VShopWeb/VShopWeb.Products/Services/Contracts/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using VShopWeb.Products.Models;
+
+namespace VShopWeb.Products.Services.Contracts;
+
+public interface IProductService 
+{
+    public Task<Product> Create(Product product);
+    public Task<Product> GetById(int id);
+    public Task<IEnumerable<Product>> GetAll();
+    public Task<IEnumerable<Product>> GetAllIncludeCategory();
+    public Task<Product> Update(Product product);
+    public Task<Product> Delete(string id);
+
+}
