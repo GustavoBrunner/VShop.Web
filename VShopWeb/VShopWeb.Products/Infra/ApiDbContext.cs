5eb38a8c6947b27091d8b0dc6321443dd7207a27
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-06 11:50:43 -0300

    Test

---
+++ VShopWeb/VShopWeb.Products/Infra/ApiDbContext.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShopWeb.Products.Models;
+
+namespace VShopWeb.Products.Infra;
+
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder mB)
+    {
+        //Products
+        mB.Entity<Product>()
+            .ToTable("product");
+        mB.Entity<Product>()
+            .HasKey(p => p.Id);
+        mB.Entity<Product>().Property(p => p.Id)
+            .HasColumnName("pk_id")
+            .HasMaxLength(100);
+        mB.Entity<Product>()
+            .Property(p => p.Name)
+            .HasColumnName("name")
+            .HasMaxLength(100)
+            .IsRequired();
+        mB.Entity<Product>()
+            .Property(p => p.Description)
+            .HasColumnName("description")
+            .HasMaxLength(255)
+            .IsRequired(false);
+        mB.Entity<Product>()
+            .Property(p => p.Price)
+            .HasColumnName("price")
+            .HasPrecision(10,2)
+            .IsRequired();
+        mB.Entity<Product>()
+            .HasOne(p => p.Category)
+            .WithMany(p => p.Products);
+        mB.Entity<Product>()
+            .Property(p => p.CategoryId)
+            .HasColumnName("fk_category_id")
+            .HasMaxLength(100);
+
+        //Categories
+
+        mB.Entity<Category>()
+            .ToTable("category");
+        mB.Entity<Category>()
+            .HasKey(p => p.Id);
+        mB.Entity<Category>()
+            .Property(p => p.Id)
+            .HasColumnName("pk_id")
+            .HasMaxLength(100);
+        mB.Entity<Category>()
+            .Property(p => p.Name)
+            .HasColumnName("name")
+            .HasMaxLength(100);
+        mB.Entity<Category>()
+            .Property(p => p.Description)
+            .HasColumnName("description")
+            .HasMaxLength(255);
+        
+
+    }
+
+}
