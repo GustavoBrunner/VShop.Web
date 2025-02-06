8329a1e608e101f1e1560c4a639a3138d5d1cde2
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-06 14:53:25 -0300

    Exception handling added

---
+++ VShopWeb/VShopWeb.Products/Exceptions/ProductEntityException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace VShopWeb.Products.Exceptions;
+
+public class ProductEntityException : Exception
+{
+    public ProductEntityException()
+    {
+    }
+
+    public ProductEntityException(string? message) : base(message)
+    {
+    }
+
+    public ProductEntityException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected ProductEntityException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
