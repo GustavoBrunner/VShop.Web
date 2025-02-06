c818d534b1cbe3dc5248b944259abdb55afbd533
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-06 13:56:59 -0300

    product controller added

---
+++ VShopWeb/VShopWeb.Products/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VShopWeb.Products.DTOs;
+using VShopWeb.Products.Services.Contracts;
+
+namespace VShopWeb.Products.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet("/{string:id}")]
+    public async Task<ActionResult<ProductViewDTO>> GetById(string id)
+    {
+        if (id == null)
+            return BadRequest("Id can not be null!");
+
+        var entity = await _productService.GetById(id);
+        if(entity == null)
+            return NotFound("Product not found on System!");
+
+        return Ok(entity);
+    }
+
+}
