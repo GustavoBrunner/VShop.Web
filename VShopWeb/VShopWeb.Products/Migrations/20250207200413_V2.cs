a1941906985c1a245081b41ce4d4d2b8b5638dce
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-07 18:14:39 -0300

    Migrations V2 added

---
+++ VShopWeb/VShopWeb.Products/Migrations/20250207200413_V2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShopWeb.Products.Migrations
+{
+    public partial class V2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "image_url",
+                table: "product",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "image_url",
+                table: "product");
+        }
+    }
+}
