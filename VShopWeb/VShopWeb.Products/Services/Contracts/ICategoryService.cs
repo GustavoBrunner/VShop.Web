a0e6b356dbb536883e9576dd7ba45fa9eace309d
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-07 10:51:06 -0300

    Category services contract and implementation for CRUD

---
+++ VShopWeb/VShopWeb.Products/Services/Contracts/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using VShopWeb.Products.DTOs;
+
+namespace VShopWeb.Products.Services.Contracts;
+
+public interface ICategoryService
+{
+    Task<List<CategoryViewDTO>> GetAllCategories();
+
+    Task<CategoryViewDTO> GetById(string id);
+
+    Task<List<CategoryViewDTO>> GetAllIncludeProduct(string id);
+
+    Task<CategoryViewDTO> Create(CategoryDTO entity);
+
+    Task<CategoryViewDTO> Update(CategoryDTO entity);
+
+    Task<CategoryViewDTO> Delete(string id);
+    
+
+}
