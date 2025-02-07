d6ad4c45f0e75cf5c3ab6dfd6055bd69b0239c89
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-07 10:49:02 -0300

    Exception handling for category entity added

---
+++ VShopWeb/VShopWeb.Products/Exceptions/CategoryEntityException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace VShopWeb.Products.Exceptions;
+
+public class CategoryEntityException : Exception
+{
+    public CategoryEntityException()
+    {
+    }
+
+    public CategoryEntityException(string? message) : base(message)
+    {
+    }
+
+    public CategoryEntityException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected CategoryEntityException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
