989ddef5070088c6bc6fe36f6e85b2d1459dc198
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-08 12:49:22 -0300

    Misspeled unitofwork delete

---
+++ VShopWeb/Frontend/Models/ProductViewDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Frontend.Models;
+
+public record ProductViewDTO(
+    
+    string Name,
+    
+    string Description,
+
+    decimal Price,
+
+    long Stock,
+
+    string ImageUrl,
+
+    string CategoryName){
+
+}
