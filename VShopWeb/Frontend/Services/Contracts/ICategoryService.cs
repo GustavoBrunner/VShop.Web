6675ce4af72f2e96a79552d94572824e999db2be
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-12 16:46:23 -0300

    Category service interface and implementation finished

---
+++ VShopWeb/Frontend/Services/Contracts/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Frontend.Models.Dtos;
+
+namespace Frontend.Services.Contracts;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<CategoryNoProductsViewDTO>> GetAllCategories();
+    Task<IEnumerable<CategoryViewDTO>> GetCategoryWithProducts(string categoryName);
+    Task<CategoryViewDTO> CreateCategory(CategoryDTO newCategory);
+    Task<CategoryViewDTO> GetCategoryById(string id);
+    Task<CategoryViewDTO> UpdateCategory(CategoryDTO category);
+    Task<CategoryViewDTO> DeleteCategory(string id);
+}
