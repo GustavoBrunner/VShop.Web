22d4f442775fb158e2f18a4cd10490c6c4500cd2
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-13 17:51:04 -0300

    Services exception handler

---
+++ VShopWeb/Frontend/Exceptions/ServiceExceptionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+namespace Frontend.Exceptions;
+
+public class ServiceExceptionHandler
+{
+    public static async Task<T> ExceptionHandler<T>(HttpResponseMessage response, JsonSerializerOptions _jsonSerializerOptions)
+    {
+        T dto;
+        if (response.IsSuccessStatusCode)
+        {
+            var responseApi = await response.Content.ReadAsStreamAsync();
+
+            dto = await JsonSerializer
+                .DeserializeAsync<T>(responseApi, _jsonSerializerOptions);
+ 
+        }
+        else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+        {
+            throw new ProductApiBadRequestReturnException("Bad request response received from api!");
+        }
+        else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+        {
+            throw new ProductApiNotFoundReturnException("Not found response reveived from api!");
+        }
+        else
+            throw new ProductApiInternalServerReturnException("Internal server response received from api!");
+
+        return dto;
+    }
+}
