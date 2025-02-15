9aa62ca94311e2235d952d425fd6451282ab3274
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-15 19:00:11 -0300

    Application Identity configuration and db context access implemented

--- VShopWeb/IdentityService/Program.cs
+++ VShopWeb/IdentityService/Program.cs
@@ -1,9 +1,44 @@
+using IdentityService.Configuration;
+using IdentityService.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var connectionString = builder.Configuration.GetConnectionString(nameof(AppDbContext));
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+        options.UseMySql(connectionString,
+            ServerVersion.AutoDetect(connectionString))
+);
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+
+var builderIdentityServer = builder.Services.AddIdentityServer(
+        options =>
+        {
+            options.Events.RaiseFailureEvents = true;
+            options.Events.RaiseSuccessEvents = true;
+            options.Events.RaiseInformationEvents = true;
+            options.Events.RaiseErrorEvents = true;
+            options.EmitStaticAudienceClaim = true;
+        })
+    .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();
+
+builderIdentityServer.AddDeveloperSigningCredential();
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,6 +52,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
