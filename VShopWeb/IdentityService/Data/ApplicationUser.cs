98fbb0b90d0ffc58b5c7fae2d901b16960704b26
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-02-18 14:40:29 -0300

    test

--- VShopWeb/IdentityService/Data/ApplicationUser.cs
+++ VShopWeb/IdentityService/Data/ApplicationUser.cs
@@ -8,10 +8,6 @@ public class ApplicationUser : IdentityUser
     public string FirstName { get; set; } = String.Empty;
     public string LastName { get; set; } = String.Empty;
 
-
-
-
-
     [NotMapped]
     public string FullName { get => $"{FirstName} {LastName}"; }
 }
